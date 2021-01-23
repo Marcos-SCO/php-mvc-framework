865c0d53aada2c07e73ece2cf1a3a6a356753651
Marcos-SCO <marcos_sco@outlook.com>
2021-01-23 12:52:54 -0300

    Request

---
+++ app/core/Request.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Core;
+
+class Request
+{
+    public function getPath()
+    {
+        // $path = $_SERVER['REQUEST_URI'] ?? '/';
+        $path = $_SERVER['PATH_INFO'] ?? '/';
+        $position = strpos($path, '?');
+
+        if (!$position) {
+            return $path;
+        }
+        // Removes query string and returns only the path
+        return substr($path, 0, $position);
+    }
+
+    public function getMethod()
+    {
+        return strtolower($_SERVER['REQUEST_METHOD']);
+    }
+}
