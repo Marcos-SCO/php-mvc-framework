43e8c3953e0a68f6f212c7e00489376ddeae008d
Marcos-SCO <marcos_sco@outlook.com>
2021-01-24 18:55:52 -0300

    Register email properties

--- app/models/RegisterModel.php
+++ app/models/RegisterModel.php
@@ -4,5 +4,10 @@ namespace App\Models;
 
 class RegisterModel 
 {
-    
+    // Properties to validate
+    public string $first_name;
+    public string $last_name;
+    public string $email;
+    public string $password;
+    public string $password_confirm;
 }
