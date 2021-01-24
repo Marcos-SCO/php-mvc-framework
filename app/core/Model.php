57aaf7fb8ebdf1b22fdb7b345fa19be2beae7c5b
Marcos-SCO <marcos_sco@outlook.com>
2021-01-24 19:17:17 -0300

    loading received data and assigning

--- app/core/Model.php
+++ app/core/Model.php
@@ -6,6 +6,12 @@ abstract class Model
 {
     public function loadData($data)
     {
+        foreach ($data as $key => $value) {
+            if (property_exists($this, $key)) {
+                // assign received values to corresponding properties 
+                $this->{$key} = $value;
+            }
+        }
     }
 
     public function validate()
