77a029dcb222fc83576710bfa6affe0bbf9b9791
Marcos-SCO <marcos_sco@outlook.com>
2021-01-24 20:43:33 -0300

    form begin and end methods

--- app/core/form/Form.php
+++ app/core/form/Form.php
@@ -2,7 +2,28 @@
 
 namespace App\Core\Form;
 
+use App\Core\Model;
 
 class Form
 {
+    public static function begin($action, $method)
+    {
+        echo sprintf(
+            '<form action="%s" method="%s">',
+            $action,
+            $method
+        );
+
+        return new Form();
+    }
+
+    public static function end()
+    {
+        return '</form>';
+    }
+
+    public function field(Model $model, $attribute)
+    {
+        return new Field($model, $attribute);
+    }
 }
--- app/views/register.php
+++ app/views/register.php
@@ -1,32 +1,34 @@
 <h1>Create an account</h1>
 
-<form action="<?= $_ENV['BASE'] ?>/register" method="post">
-    <div class="row">
-        <div class="col">
-            <div class="mb-3">
-                <label for="first_name" class="form-label">First name</label>
-                <input type="text" class="form-control" name="first_name" id="first_name" aria-describedby="first_name">
-            </div>
-        </div>
-        <div class="col">
-            <div class="mb-3">
-                <label for="last_name" class="form-label">Last name</label>
-                <input type="text" class="form-control" name="last_name" id="last_name" aria-describedby="last_name">
-            </div>
+<?= App\Core\Form\Form::begin($_ENV['BASE'] . '/register', 'post') ?>
+<!-- <form action="<?//= $_ENV['BASE'] ?>/register" method="post"> -->
+<div class="row">
+    <div class="col">
+        <div class="mb-3">
+            <label for="first_name" class="form-label">First name</label>
+            <input type="text" class="form-control" name="first_name" id="first_name" aria-describedby="first_name">
         </div>
     </div>
-    <div class="mb-3">
-        <label for="email" class="form-label">Email</label>
-        <input type="text" class="form-control" name="email" id="email" aria-describedby="email">
+    <div class="col">
+        <div class="mb-3">
+            <label for="last_name" class="form-label">Last name</label>
+            <input type="text" class="form-control" name="last_name" id="last_name" aria-describedby="last_name">
+        </div>
     </div>
-    <div class="mb-3">
-        <label for="password" class="form-label">password</label>
-        <textarea type="password" class="form-control" name="password" id="password" aria-describedby="password"></textarea>
-    </div>
-    <div class="mb-3">
-        <label for="confirm_password" class="form-label">Confirm Password</label>
-        <textarea type="password" class="form-control" name="password_confirm" id="password_confirm" aria-describedby="password_confirm"></textarea>
-    </div>
+</div>
+<div class="mb-3">
+    <label for="email" class="form-label">Email</label>
+    <input type="text" class="form-control" name="email" id="email" aria-describedby="email">
+</div>
+<div class="mb-3">
+    <label for="password" class="form-label">password</label>
+    <textarea type="password" class="form-control" name="password" id="password" aria-describedby="password"></textarea>
+</div>
+<div class="mb-3">
+    <label for="confirm_password" class="form-label">Confirm Password</label>
+    <textarea type="password" class="form-control" name="password_confirm" id="password_confirm" aria-describedby="password_confirm"></textarea>
+</div>
 
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<button type="submit" class="btn btn-primary">Submit</button>
+<!-- </form> -->
+<?= App\Core\Form\Form::end() ?>
