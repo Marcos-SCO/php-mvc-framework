94ac415bc596bc5be02fc84e483ad8a4f4f27abd
Marcos-SCO <marcos_sco@outlook.com>
2021-01-24 20:27:19 -0300

    error methods

--- app/core/Model.php
+++ app/core/Model.php
@@ -74,4 +74,14 @@ abstract class Model
             self::RULE_MATCH => 'Esse campo deve ter o mesmo valor de {match}',
         ];
     }
+
+    public function hasError($attribute)
+    {
+        return $this->errors[$attribute] ?? false;
+    }
+
+    public function getFirstError($attribute)
+    {
+        return $this->errors[$attribute][0] ?? '';
+    }
 }
