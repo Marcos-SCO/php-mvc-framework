2fdc3cc7bcfc41a2497c879b1249968de4bd05cd
Marcos-SCO <marcos_sco@outlook.com>
2021-01-24 19:53:22 -0300

    add error and match messages

--- app/core/Model.php
+++ app/core/Model.php
@@ -10,6 +10,8 @@ abstract class Model
     public const RULE_MAX = 'max';
     public const RULE_MATCH = 'match';
 
+    public array $errors = [];
+
     public function loadData($data)
     {
         foreach ($data as $key => $value) {
@@ -24,6 +26,34 @@ abstract class Model
 
     public function validate()
     {
-        
+        foreach ($this->rules() as $attribute => $rules) {
+            $value = $this->{$attribute};
+            foreach ($rules as $rule) {
+                $ruleName = $rule;
+                if (!is_string($ruleName)) {
+                    $ruleName = $rule[0];
+                }
+                if ($ruleName === self::RULE_REQUIRED && !$value) {
+                    $this->addError($attribute, self::RULE_REQUIRED);
+                }
+            }
+        }
+    }
+
+    public function addError(string $attribute, string $rule)
+    {
+        $message = $this->errorMessages()[$rule] ?? '';
+        $this->errors[$attribute][] = $message;
+    }
+
+    public function errorMessages()
+    {
+        return [
+            self::RULE_REQUIRED => 'Esse campo é requirido',
+            self::RULE_EMAIL => 'Esse campo deve ter um e-mail válido',
+            self::RULE_MIN => 'Tamanho minimo do campo deve ser de {min}',
+            self::RULE_MAX => 'Tamanho máximo do campo deve ser de {min}',
+            self::RULE_MATCH => 'Esse campo deve ter o mesmo valor de {match}',
+        ];
     }
 }
