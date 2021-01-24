2070000f677d3a22526c4a8f9f772a61a2692f4e
Marcos-SCO <marcos_sco@outlook.com>
2021-01-24 20:46:43 -0300

    field constructor, and properties

--- app/core/form/Field.php
+++ app/core/form/Field.php
@@ -2,6 +2,16 @@
 
 namespace App\Core\Form;
 
+use App\Core\Model;
+
 class Field
 {
+    public Model $model;
+    public string $attribute;
+
+    public function __construct(Model $model, string $attribute)
+    {
+        $this->model = $model;
+        $this->attribute = $attribute;
+    }
 }
