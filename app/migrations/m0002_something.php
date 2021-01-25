81a86c77552cd0b76016b70be13c27e424ef7c95
Marcos-SCO <marcos_sco@outlook.com>
2021-01-25 15:33:26 -0300

    migrations

---
+++ app/migrations/m0002_something.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Migrations;
+
+class m0002_something
+{
+    public function up()
+    {
+        echo "applying migration";
+    }
+    public function down()
+    {
+        echo "down migration";
+    }
+}
