0a82ccfb34302611675cbc61353eaf2393d57139
Marcos-SCO <marcos_sco@outlook.com>
2021-01-25 17:16:27 -0300

    migrations

---
+++ app/core/Database.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Core;
+
+use PDO;
+
+class Database
+{
+    public PDO $pdo;
+    public function __construct(array $config)
+    {
+        $dsn = $config['dsn'] ?? '';
+        $user = $config['user'] ?? '';
+        $password = $config['password'] ?? '';
+        $this->pdo = new PDO($dsn, $user, $password);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function applyMigrations()
+    {
+        $this->createMigrationsTable();
+        $appliedMigrations = $this->getAppliedMigrations();
+        $files = scandir(Application::$ROOT_DIR . '/migrations');
+        $toApplyMigrations = array_diff($files, $appliedMigrations);
+        foreach ($toApplyMigrations as $migration) {
+            if ($migration === '.' || $migration === '..') {
+                continue;
+            }
+
+            require_once Application::$ROOT_DIR . '/migrations/' . $migration;
+            $className = 'App\Migrations\\' . pathinfo($migration, PATHINFO_FILENAME);
+            $instance = new $className();
+            echo "Applying migration $migration" . PHP_EOL;
+            $instance->up();
+        }
+    }
+
+    public function createMigrationsTable()
+    {
+        $this->pdo->exec("CREATE TABLE IF NOT EXISTS migrations (
+           id INT AUTO_INCREMENT PRIMARY KEY,
+           migration VARCHAR(255),
+           created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        ) ENGINE=INNODB;");
+    }
+
+    public function getAppliedMigrations()
+    {
+        $stmt = $this->pdo->prepare("SELECT migration from migrations");
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_COLUMN);
+    }
+}
--- app/migrations.php
+++ app/migrations.php
@@ -1,2 +1,22 @@
-<?php 
+<?php
 
+use App\Core\Application;
+
+$dirname = __DIR__;
+
+require_once $dirname . "/vendor/autoload.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable($dirname, '.env');
+$dotenv->load();
+
+$config = [
+    'db' => [
+        'dsn' => $_ENV['DB_DSN'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD'],
+    ],
+];
+
+$app = new Application(__DIR__, $config['db']);
+
+$app->db->applyMigrations();
