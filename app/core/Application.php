dd76636699f87a62f319437f6eaea1b1931c115d
Marcos-SCO <marcos_sco@outlook.com>
2021-01-25 16:11:53 -0300

    verify params to connect

--- app/core/Application.php
+++ app/core/Application.php
@@ -6,28 +6,44 @@ class Application
 {
     public Router $router;
     public Request $request;
+    public Database $db;
     public static Application $app;
     public Controller $controller;
-    
-    public function __construct()
+
+    public function __construct(array $config = [])
     {
         self::$app = $this;
         $this->request = new Request();
         $this->router = new Router($this->request);
+
+        if ($this->allParamsForConnection($config)) {
+            $this->db = new Database($config);
+        }
     }
-    
-    public function get($path, $callback) {
-        $this->router->get($path,$callback);
+
+    public function allParamsForConnection(array $config)
+    {
+        foreach ($config as $key => $value) {
+            if (empty($value) && $key != 'password') return false;
+        }
+        return true;
     }
-    
-    public function post($path, $callback) {
-        $this->router->post($path,$callback);
+
+    public function get($path, $callback)
+    {
+        $this->router->get($path, $callback);
     }
-    
-    public function renderView($view, $params = []) {
+
+    public function post($path, $callback)
+    {
+        $this->router->post($path, $callback);
+    }
+
+    public function renderView($view, $params = [])
+    {
         return $this->router->renderView($view, $params);
     }
-    
+
     public function run()
     {
         echo $this->router->resolve();
@@ -35,7 +51,7 @@ class Application
 
     /**
      * Get the value of controller
-     */ 
+     */
     public function getController()
     {
         return $this->controller;
@@ -45,7 +61,7 @@ class Application
      * Set the value of controller
      *
      * @return  self
-     */ 
+     */
     public function setController($controller)
     {
         $this->controller = $controller;
