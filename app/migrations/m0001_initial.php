cefcbc5fcbb446fd899ccb257deb6c27595c3dd8
Marcos-SCO <marcos_sco@outlook.com>
2021-01-25 17:54:45 -0300

    up and down

--- app/migrations/m0001_initial.php
+++ app/migrations/m0001_initial.php
@@ -2,14 +2,30 @@
 
 namespace App\Migrations;
 
+use App\Core\Application;
+
 class m0001_initial
 {
     public function up()
     {
-        echo "applying migration";
+        $db = Application::$app->db;
+
+        $sql = "CREATE TABLE users (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            email VARCHAR(255) NOT NULL,
+            first_name VARCHAR(255) NOT NULL,
+            last_name VARCHAR(255) NOT NULL,
+            status TINYINT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        ) ENGINE=INNODB;";
+
+        $db->pdo->exec($sql);
     }
+
     public function down()
     {
-        echo "down migration";
+        $db = Application::$app->db;
+        $sql = "DROP TABLE users";
+        $db->pdo->exec($sql);
     }
 }
