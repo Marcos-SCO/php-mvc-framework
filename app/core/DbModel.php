02421639679500316cb414081ac57f86c20dbbb9
Marcos-SCO <marcos_sco@outlook.com>
2021-01-26 09:41:59 -0300

    attributes, prepare, save methods

--- app/core/DbModel.php
+++ app/core/DbModel.php
@@ -6,7 +6,19 @@ abstract class DbModel extends Model
 {
     abstract public function tableName(): string;
 
+    abstract public function attributes(): array;
+
     public function save()
     {
+        $tableName = $this->tableName;
+        $attributes = $this->attributes;
+        $params = array_map(fn($attr) => ":$attr", $attributes);
+        $stmt = self::prepare('INSERT INTO' . $tableName . ' (' . implode(',', $attributes) . ') VALUES (' . implode(',', $params) . ')');
+        var_dump($stmt, $params, $attributes);
+    }
+
+    public static function prepare($sql)
+    {
+        Application::$app->db->pdo->prepare($sql);
     }
 }
