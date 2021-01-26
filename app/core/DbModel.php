26486d7492690cdb7e7bf7deaeee6d4268377b5b
Marcos-SCO <marcos_sco@outlook.com>
2021-01-26 10:20:17 -0300

    bind values

--- app/core/DbModel.php
+++ app/core/DbModel.php
@@ -10,15 +10,39 @@ abstract class DbModel extends Model
 
     public function save()
     {
-        $tableName = $this->tableName;
-        $attributes = $this->attributes;
-        $params = array_map(fn($attr) => ":$attr", $attributes);
-        $stmt = self::prepare('INSERT INTO' . $tableName . ' (' . implode(',', $attributes) . ') VALUES (' . implode(',', $params) . ')');
-        var_dump($stmt, $params, $attributes);
+        $tableName = $this->tableName();
+        $attributes = $this->attributes();
+        $params = array_map(fn ($attr) => ":$attr", $attributes);
+        $stmt = self::prepare('INSERT INTO ' . $tableName . ' (' . implode(',', $attributes) . ') VALUES (' . implode(',', $params) . ')');
+
+        foreach ($attributes as $attribute) {
+            // $stmt->bindValue("{:$attribute}", $this->{$attribute});
+            self::bind($stmt, "{:$attribute}", $this->{$attribute});
+        }
     }
 
     public static function prepare($sql)
     {
-        Application::$app->db->pdo->prepare($sql);
+        return Application::$app->db->pdo->prepare($sql);
+    }
+
+    public static function bind($stmt, $param, $value, $type = null)
+    {
+        if (is_null($type)) {
+            switch ($value) {
+                case is_int($value):
+                    $type = \PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = \PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = \PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = \PDO::PARAM_STR;
+            }
+        }
+        return $stmt->bindValue($param, $value, $type);
     }
 }
