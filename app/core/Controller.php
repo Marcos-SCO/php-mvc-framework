c3ee9041d4a4ecaab25cfad36c2b90b42f84fa29
Marcos-SCO <marcos_sco@outlook.com>
2021-01-28 10:10:19 -0300

    register and get Middlewares

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -2,9 +2,18 @@
 
 namespace App\Core;
 
+use App\Core\Middleware\BaseMiddleware;
+
 class Controller
 {
     public string $layout = 'main';
+    public string $action = '';
+
+    /**
+     * @var \App\Core\Middleware\BaseMiddleware
+     */
+    protected array $middlewares = [];
+
     public function setLayout($layout)
     {
         $this->layout = $layout;
@@ -13,5 +22,20 @@ class Controller
     public function render($view, $params = [])
     {
         return Application::$app->renderView($view, $params);
+    }
+
+    public function registerMiddleware(BaseMiddleware $middleware)
+    {
+        $this->middlewares[] = $middleware;
+    }
+
+    /**
+     * Get the value of middlewares
+     *
+     * @return  \App\Core\Middleware\BaseMiddleware
+     */ 
+    public function getMiddlewares(): array
+    {
+        return $this->middlewares;
     }
 }
